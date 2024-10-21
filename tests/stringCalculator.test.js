fda1c8b5042cdc2f8e9d27f88aae0f27073f0451
Riddhesh Desai <riddheshdesai56@gmail.com>
2024-10-21 14:02:27 +0530

    test: add test for newlines as delimiters

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -17,4 +17,8 @@ describe("String Calculator", () => {
   test("returns the sum of multiple numbers separated by a comma", () => {
     expect(add("1,2,3,4,5")).toBe(15);
   });
+
+  test("returns the sum when numbers are separated by commas or newlines", () => {
+    expect(stringCalculator.add("1\n2,3")).toBe(6);
+  });
 });
