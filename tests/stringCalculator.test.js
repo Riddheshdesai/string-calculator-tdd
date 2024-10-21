a67ae4180c943b16af9de1e1f43a4a2a4bd2dffd
Riddhesh Desai <riddheshdesai56@gmail.com>
2024-10-21 13:52:28 +0530

    test: add test for single number input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,8 +1,12 @@
 // stringCalculator.test.js
-import { add } from '../src/utils/stringCalculator'
+import { add } from "../src/utils/stringCalculator";
 
-describe('String Calculator', () => {
-  test('return 0 for an empty string', () => {
-    expect(add('')).toBe(0);
+describe("String Calculator", () => {
+  test("return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+
+  test("returns the number itself for a single number input", () => {
+    expect(add("1")).toBe(1);
   });
 });
