f379857c00b040a88c12fcea9569ec3bd5995b35
Riddhesh Desai <riddheshdesai56@gmail.com>
2024-10-21 14:09:20 +0530

    refactor: clean up code for readability

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,12 +1,13 @@
 export function add(numbers) {
-  if (numbers == "") return 0;
+  let inputString = numbers;
+  if (numbers === "") return 0;
   let delimiter = /[\n,]/;
   if (numbers.startsWith("//")) {
-    const delimiterEnd = numbers.indexOf("\n");
-    delimiter = new RegExp(numbers.substring(2, delimiterEnd));
-    numbers = numbers.substring(delimiterEnd + 1);
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(`${parts[0][2]}`);
+    inputString = parts[1];
   }
 
-  const numberArray = numbers.split(delimiter);
-  return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
+  const numbbers = inputString.split(delimiter).map(Number);
+  return numbbers.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
