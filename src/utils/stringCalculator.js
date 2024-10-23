d3f371635c3a0f1ebfadcbdda49a9fde303045d6
Riddhesh Desai <riddheshdesai56@gmail.com>
2024-10-23 17:20:13 +0530

    feat: implement the function that extract number are 1000 or above

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -3,17 +3,33 @@ export function add(numbers) {
   if (numbers === "") return 0;
   let delimiter = /[\n,]/;
   if (numbers.startsWith("//")) {
-    const parts = numbers.split("\n");
-    delimiter = new RegExp(`${parts[0][2]}`);
-    inputString = parts[1];
+    const { extractDelimeterValue, extractInputValue } = extractDelimeter(
+      numbers,
+      delimiter,
+      inputString
+    );
+    delimiter = extractDelimeterValue;
+    inputString = extractInputValue;
   }
 
   const numberArray = inputString.split(delimiter);
+  negetiveNumbers(numberArray);
+
+  return numberArray
+    .filter((number) => number < 1000)
+    .reduce((sum, num) => sum + parseInt(num, 10), 0);
+}
+function extractDelimeter(numbers, delimiter, inputString) {
+  const parts = numbers.split("\n");
+  delimiter = new RegExp(`${parts[0][2]}`);
+  inputString = parts[1];
+  return { extractDelimeterValue: delimiter, extractInputValue: inputString };
+}
+
+function negetiveNumbers(numberArray) {
   const negatives = numberArray.filter((num) => parseInt(num, 10) < 0);
 
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
   }
-
-  return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
